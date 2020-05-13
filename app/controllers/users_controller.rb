aab00ed71827471520b5fafa33b5b31fe512ae2e
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-05-13 13:26:17 +0900

    userのcontrollerとviewを整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,17 +22,14 @@ class UsersController < ApplicationController
   end
 
   # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        redirect_to user_path(@user.id), notice: 'User was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render :new
       end
     end
   end
@@ -69,6 +66,7 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest, :image, :profile)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation, :image, :profile)
     end
 end
