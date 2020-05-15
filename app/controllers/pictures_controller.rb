6645376d3c64e83f991427ebe47711df10394975
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-05-15 17:55:43 +0900

    ユーザ登録語画面を調整

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,5 @@
 class PicturesController < ApplicationController
-  before_action :login_check
+  before_action :login_check, only: [:index, :new, :show, :edit, :update, :destroy]
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
 
   def index
