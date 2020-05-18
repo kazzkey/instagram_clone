f31089d91c4d44d363705a12407763adea8c1e76
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-05-18 14:46:47 +0900

    mailerの送り先を投稿者に設定

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -1,6 +1,6 @@
 class PictureMailer < ApplicationMailer
   def picture_mail(picture)
     @picture = picture
-    mail to: "k_kawano86@yahoo.co.jp", subject: "画像投稿確認メール"
+    mail to: @picture.user.email, subject: "画像投稿確認メール"
   end
 end
