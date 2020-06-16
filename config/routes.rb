dc2be3702bfd826280fc22a34e99051b28d2b8c6
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-06-16 10:58:48 +0900

    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pictures#index'
+  root to: 'sessions#new'
   resources :favorites, only: [:create, :destroy]
   resources :pictures do
     collection do
